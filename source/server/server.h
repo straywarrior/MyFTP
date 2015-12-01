c7ea8bebf46fc56ded38e3ad8e845cb274d94f9a
fzt <fengzhoutian@hotmail.com>
2015-12-02 00:21:19 +0800

    add server header

---
+++ source/server/server.h
@@ -0,0 +1,13 @@
+#ifndef _MYFTP_SERVER_H
+#define _MYFTP_SERVER_H value
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+#endif

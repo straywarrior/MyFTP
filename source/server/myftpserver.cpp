40d63036b52bd0cebabf9404398b1917ec5355f2
冯周天 <fengzhoutian@Feng-Zhoutian-MacBook-Air.local>
2015-12-02 01:17:13 +0800

    add cmakelist

---
+++ source/server/myftpserver.cpp
@@ -0,0 +1,15 @@
+/*
+ * myftpserver.cpp
+ * @Author:     StrayWarrior
+ * @Date:       2015-12-02
+ * @Email:      i@straywarrior.com
+ * Copyright (C) 2015 StrayWarrior
+ *
+ */
+
+#include "myftpserver.h"
+
+int main(int argc, char * argv[]){
+    return 0;
+}
+

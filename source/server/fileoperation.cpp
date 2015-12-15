b3fbf7f677b14f7b2c067d899b6253cfd73a35d8
fzt <fengzhoutian@hotmail.com>
2015-12-15 18:03:32 +0800

    Add path simplifier

--- source/server/fileoperation.cpp
+++ source/server/fileoperation.cpp
@@ -8,6 +8,43 @@
  */
 
 #include "fileoperation.h"
+#include <string>
+#include <vector>
+
+static void clean_path(char * path){
+    std::string path_str = std::string(path);
+    std::vector<std::string> dir_vec;
+    std::string sec;
+
+    path_str += "/";
+    for (int i = 0; i < path_str.size(); ++i){
+        if (path[i] == '/'){
+            if (sec.empty()) continue;
+            if (sec == "."){
+                // Do Nothing
+            }else if (sec == ".."){
+                if (dir_vec.size())
+                    dir_vec.pop_back();
+            }else{
+                dir_vec.push_back(sec);
+            }
+            sec = "";
+        }else{
+            sec += path[i];
+        }
+    }
+
+    std::string result;
+    if (dir_vec.empty()){
+        strcpy(path, "/");
+    }else{
+        for (int i = 0; i < dir_vec.size(); ++i){
+            result += ("/" + dir_vec[i]);
+        }
+        strcpy(path, result.c_str());
+    }
+
+}
 
 int init_working_dir(myftpserver_worker_t * worker_t){
     return 0;
@@ -16,19 +53,16 @@ int init_working_dir(myftpserver_worker_t * worker_t){
 int change_dir(myftpserver_worker_t * worker_t, const char * pathname);
 int get_cur_path(myftpserver_worker_t * worker_t, char * result){
     result[0] = '\0';
-    strcat(result, "/");
     strcat(result, worker_t->reladir);
     return 0;
 }
 int get_absolute_path(myftpserver_worker_t * worker_t, char * result){
     result[0] = '\0';
     strcat(result, worker_t->rootdir);
-    strcat(result, "/");
     strcat(result, worker_t->reladir);
     return 0;
 }
-int list_dir(myftpserver_worker_t * worker_t, char * result);
-int open_dir(char * path);
+int list_dir(myftpserver_worker_t * worker_t);
+DIR * open_dir(char * path);
 int close_working_dir(myftpserver_worker_t * worker_t);
-
 

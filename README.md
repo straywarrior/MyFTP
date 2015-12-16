82afd7195f92c72c9187b68e707999814ddaf60b
Feng Zhoutian <fengzhoutian@hotmail.com>
2015-12-16 20:55:12 +0800

    Add Client-side STOR, RETR. First Usable Version!

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # MyFTP
+2015-2016学年秋季学期 计算机网络原理大实验
+
 A simple FTP server for practice.
+RFC 959 minimum support.
 
 Working on Linux only.
 Support for Windows may come in the future.
+
+A simple FTP server for practice.
--- source/client/connection.h
+++ source/client/connection.h
@@ -29,7 +29,7 @@ enum class FTPCMD{
     UNKNOWN, ERROR, UNIMPL
 };
 
-FTPCMD read_command(int connection, char * arg_buf); 
+FTPCMD read_command(int ctl_conn, char * arg_buf); 
 FTPCMD parse_command(const char * read_buf, char * arg_buf);
 
 int split_arg(const char * cmd_buf, char * arg_bug);
@@ -39,19 +39,22 @@ int split_arg(const char * cmd_buf, char * arg_bug);
  */
 
 #define prepare_reply(send_buf, reply_fmt, ...) sprintf(send_buf, reply_fmt, __VA_ARGS__)
-int send_reply(int connection, const char * send_buf);
-int send_reply(int connection, const char * send_buf, int len);
+int send_reply(int ctl_conn, const char * send_buf);
+int send_reply(int ctl_conn, const char * send_buf, int len);
 
-int recv_reply(int connection, char * recv_buf);
-int recv_and_show_reply(int connection);
-int parse_reply(int connection, char * recv_buf);
+int recv_reply(int ctl_conn, char * recv_buf);
+int recv_and_show_reply(int ctl_conn);
+int parse_reply(int ctl_conn, char * recv_buf);
 
 /*
  * Data Connection
  */
-int open_data_connection(int connection, struct sockaddr_in * data_sock_addr, unsigned int v4addr, unsigned int port);
-int close_data_connection(int connection, int data_conn);
-int open_and_send_data_port(int connection, struct sockaddr_in * data_sock_addr, unsigned int v4addr = 0);
+int open_data_connection(int ctl_conn, struct sockaddr_in * data_sock_addr, unsigned int v4addr, unsigned int port);
+int close_data_connection(int ctl_conn, int data_conn);
+int open_and_send_data_port(int ctl_conn, struct sockaddr_in * data_sock_addr, unsigned int v4addr = 0);
 int recv_and_show_data(int data_conn, struct sockaddr_in * data_sock_addr);
+
+int store_file(int ctl_conn, int data_conn, struct sockaddr_in * data_sock_addr, const char * filename);
+int retrieve_file(int ctl_conn, int data_conn, struct sockaddr_in * data_sock_addr, const char * filename);
 
 #endif /* !_MYFTP_CONNECTIONS_H_ */

d8812ba7f5c9b7cfb033943ce447fcb315d4ad0d
fzt <fengzhoutian@hotmail.com>
2015-12-16 21:39:13 +0800

    fix make-Makefile.bash

--- build/linux/make-Makefiles.bash
+++ build/linux/make-Makefiles.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Run this from within a bash shell
-if [ $1 = "reset" ]
+if ! [ -z "$1" ] && [ $1 = "reset" ]
 then
-    echo "rm -rf CMake* ..\n"
+    echo "rm -rf CMake* ...\n"
     rm -rf CMake*
+else
+    echo "Warning: some changes may not be updated this time.\n"
 fi
 cmake -G "Unix Makefiles" \
 	-DCMAKE_CXX_FLAGS_RELEASE=-O3 \
